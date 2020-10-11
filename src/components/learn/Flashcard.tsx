a06d3d574aae7c5152338ca1273b1bc4ed0b96d0
ELK75 <elk75@pitt.edu>
2020-10-11 01:02:56 -0400

    finish styling of flash card

--- src/components/learn/Flashcard.tsx
+++ src/components/learn/Flashcard.tsx
@@ -3,9 +3,11 @@ import React from "react";
 const Flashcard: React.FC = () => {
   return (
     <div className="flex justify-center rounded-md shadow-lg-full w-full h-full">
-      <div className="flex justify-items-start w-full mt-5 ml-5">
-        <div className="font-light text-4xl">
-          Hacerlo mal no importa: la es avanzar.
+      <div className="flex justify-items-start items-center w-full m-5">
+        <div className="font-light text-4xl text-center">
+          Hacerlo mal no importa: la es avanzar. Frase is an app that allows you
+          to learn a language seamlessly and naturally through a
+          spaced-repetition system.
         </div>
       </div>
     </div>
